79f8668bb5def5c6ba018a72fdb14db33f5351ff
Hafsah <h.jilanee@rewaatech.com>
2024-06-25 10:54:18 +0500

    initial commit

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get('flavors')
+  findAll() {
+    return 'This action returns all the coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+}
