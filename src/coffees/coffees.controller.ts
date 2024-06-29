5538d629ceaa354bdf69a41303de5fad1ff9c915
Hafsah <h.jilanee@rewaatech.com>
2024-06-29 13:27:52 +0500

    adding transform true in validation pipe

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,14 +13,15 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coffeesService.findOne(id);
+  findOne(@Param('id') id: number) {
+    //console.log(typeof id)
+    return this.coffeesService.findOne(''+id);
   }
 
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
-    this.coffeesService.create(createCoffeeDto);
-    return createCoffeeDto;
+    // console.log(createCoffeeDto instanceof CreateCoffeeDto)
+    return this.coffeesService.create(createCoffeeDto);
   }
 
   @Patch(':id')
