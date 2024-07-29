4c9d1c425c6005d909ffbe4ccba96d340bcad921
Hafsah <h.jilanee@rewaatech.com>
2024-07-29 10:58:33 +0500

    adding config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { CoffeeRatingService } from './coffee-rating/coffee-rating.service';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'pass123',
-    database: 'postgres',
-    autoLoadEntities: true,
-    synchronize: true
-  }), CoffeeRatingModule, DatabaseModule],
+  imports: [
+    CoffeesModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CoffeeRatingModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService, CoffeeRatingService],
 })
