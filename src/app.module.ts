d2fcca977443213490ff1552ce06307dd9265afb
Hafsah <h.jilanee@rewaatech.com>
2024-07-30 11:20:25 +0500

    asynchronously configure dynamic modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,19 +11,21 @@ import appConfig from 'config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
     CoffeesModule,
     ConfigModule.forRoot({
       load: [appConfig]
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
     }),
     CoffeeRatingModule,
     DatabaseModule,
