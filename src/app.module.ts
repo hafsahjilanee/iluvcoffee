41f1be870aeac98e2d956d8b4fd1f16ff73f920c
Hafsah <h.jilanee@rewaatech.com>
2024-07-30 10:54:46 +0500

    adding custom configuration files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,13 @@ import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { CoffeeRatingService } from './coffee-rating/coffee-rating.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from '@hapi/joi';
+import appConfig from 'config/app.config';
 
 @Module({
   imports: [
     CoffeesModule,
     ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        DATABASE_HOST: Joi.required(),
-        DATABASE_PORT: Joi.number().default(5432),
-      }),
+      load: [appConfig]
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
