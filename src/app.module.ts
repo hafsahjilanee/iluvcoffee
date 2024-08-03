ef57354c83c800542c70d339e8d72958eae77fe1
Hafsah <h.jilanee@rewaatech.com>
2024-08-03 12:20:38 +0500

    adding metadata to guards and interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from 'config/app.config';
 import { APP_PIPE } from '@nestjs/core';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { APP_PIPE } from '@nestjs/core';
     }),
     CoffeeRatingModule,
     DatabaseModule,
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [
